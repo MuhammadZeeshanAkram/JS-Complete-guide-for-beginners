b83178546cc59815ea7ea655f66443691013e3c7
Muhammad Zeeshan Akram <zeeshansiddiqui9790@gmail.com>
2025-03-04 03:23:04 +0500

    datatype.js completed

---
+++ datatypes.js
@@ -0,0 +1,77 @@
+//  use strict it is the way to run the js as a newer function but now  it is automatically treated as newer js code so no need to specify the use strict
+
+//alert(9+9) the popup appears that shows 18 but we can't use this in node.js environment but we use it in the browser as nodejs uses console.log instead of popup
+
+//if number too big then use bigint but practically no one uses this bigint
+
+//          Datatypes in js:
+//  Primitive Data Types(Basic Values):
+// number,
+// string,
+// boolean,
+// undefined(variable is declared but not assigned any value),
+// null(Empty or No Value),
+// Symbol (Unique value - Used for advanced cases)let id1 = Symbol("id");
+// let id2 = Symbol("id");
+// console.log(id1 === id2); // false
+// ✅ Symbols create unique values, even if they look the same.
+
+// BigInt (For very large numbers)
+
+//  Non-Primitive Data Types(Complex Values):
+//Object (Key-Value Pair)
+//let person = {
+//     name: "Zeeshan",
+//     age: 25,
+//     city: "Lahore"
+// };
+// console.log(person.name); // Zeeshan
+// ✅ Objects store multiple values using {} and key-value pairs.
+// Array (List of Values)
+// let fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits[0]); // Apple
+// ✅ Arrays store multiple values in [] and are accessed using index numbers.
+// Function (Reusable Block of Code)
+// function greet() {
+//     return "Hello, World!";
+// }
+// console.log(greet()); // "Hello, World!"
+// ✅ Functions allow us to reuse code.
+
+
+// Summary
+//null is standalone value as it is  intentionally defined null
+// console.log(typeof 25); // "number"
+// console.log(typeof "Hello"); // "string"
+// console.log(typeof true); // "boolean"
+// console.log(typeof {}); // "object"
+// console.log(typeof []); // "object" (Arrays are special objects!)
+// console.log(typeof function(){}); // "function"
+// console.log(typeof undefined);//undefined
+// console.log(typeof null);//object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
